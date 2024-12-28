a40864b0b0396fbf81c196be86fa727151903a04
ysyx_22051213-Zhang Yuchi <inchinaxiaofeng@gmail.com>
2024-12-28 14:43:47 +0800

    [add] imaxabs,imaxdiv Std C impl based on musl 1.2.5

--- ulib/mocklibc/include/inttypes.h
+++ ulib/mocklibc/include/inttypes.h
@@ -15,12 +15,12 @@ typedef struct {
     intmax_t quot, rem;
 } imaxdiv_t;
 
-// intmax_t imaxabs(intmax_t);
-// imaxdiv_t imaxdiv(intmax_t, intmax_t);
-//
+intmax_t imaxabs(intmax_t);
+imaxdiv_t imaxdiv(intmax_t, intmax_t);
+
 // intmax_t strtoimax(const char *__restrict, char **__restrict, int);
 // uintmax_t strtoumax(const char *__restrict, char **__restrict, int);
-//
+
 // intmax_t wcstoimax(const wchar_t *__restrict, wchar_t **__restrict, int);
 // uintmax_t wcstoumax(const wchar_t *__restrict, wchar_t **__restrict, int);
 
